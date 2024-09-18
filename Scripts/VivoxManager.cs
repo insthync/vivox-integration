a2b79dff55e3c0a7b7d4a50c85f3b508440521b4
insthync <ittipon.bay@gmail.com>
2024-09-18 09:07:12 +0700

    feat: WIP vivox manager

---
+++ Scripts/VivoxManager.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+namespace Insthync.UnityVivoxIntegration
+{
+    public partial class VivoxManager : MonoBehaviour
+    {
+        [SerializeField]
+        private string _server = "https://unity.vivox.com/appconfig/18966-megac-31133-udash";
+        [SerializeField]
+        private string _domain = "mtu1xp.vivox.com";
+        [SerializeField]
+        private string _issuer = "18966-megac-31133-udash";
+        [SerializeField]
+        private string _key = "A7e1Dwxhp1eKboFvj92aNbQkcFuJgF0c";
+
+        // Check to see if we're about to be destroyed.
+        static object m_Lock = new object();
+        static VivoxManager m_Instance;
+
+        /// <summary>
+        /// Access singleton instance through this propriety.
+        /// </summary>
+        public static VivoxManager Instance
+        {
+            get
+            {
+                lock (m_Lock)
+                {
+                    if (m_Instance == null)
+                    {
+                        // Search for existing instance.
+                        m_Instance = (VivoxManager)FindObjectOfType(typeof(VivoxManager));
+
+                        // Create new instance if one doesn't already exist.
+                        if (m_Instance == null)
+                        {
+                            // Need to create a new GameObject to attach the singleton to.
+                            var singletonObject = new GameObject();
+                            m_Instance = singletonObject.AddComponent<VivoxManager>();
+                            singletonObject.name = typeof(VivoxManager).ToString() + " (Singleton)";
+                        }
+                    }
+                    // Make instance persistent even if its already in the scene
+                    DontDestroyOnLoad(m_Instance.gameObject);
+                    return m_Instance;
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            if (m_Instance != this && m_Instance != null)
+            {
+                Debug.LogWarning(
+                    "Multiple VivoxManager detected in the scene. Only one VivoxManager can exist at a time. The duplicate VivoxManager will be destroyed.");
+                Destroy(this);
+            }
+#if !UNITY_SERVER
+            InitializeForClient();
+#endif
+        }
+
+        public string GenerateLoginToken(string userId, int expirationInSeconds = 90)
+        {
+            return VivoxTokenGenerator.GenerateLoginToken(_domain, _issuer, _key, userId, expirationInSeconds);
+        }
+
+        public string GenerateJoinToken(string userId, VivoxChannelType channelType, string channelId, int expirationInSeconds = 90)
+        {
+            return VivoxTokenGenerator.GenerateJoinToken(_domain, _issuer, _key, userId, channelType, channelId, expirationInSeconds);
+        }
+
+        public string GenerateJoinMutedToken(string userId, VivoxChannelType channelType, string channelId, int expirationInSeconds = 90)
+        {
+            return VivoxTokenGenerator.GenerateJoinMutedToken(_domain, _issuer, _key, userId, channelType, channelId, expirationInSeconds);
+        }
+
+        public string GenerateKickToken(string userId, string kickedUserId, VivoxChannelType channelType, string channelId, int expirationInSeconds = 90)
+        {
+            return VivoxTokenGenerator.GenerateKickToken(_domain, _issuer, _key, userId, kickedUserId, channelType, channelId, expirationInSeconds);
+        }
+
+        public string GenerateMuteToken(string userId, string mutedUserId, VivoxChannelType channelType, string channelId, int expirationInSeconds = 90)
+        {
+            return VivoxTokenGenerator.GenerateMuteToken(_domain, _issuer, _key, userId, mutedUserId, channelType, channelId, expirationInSeconds);
+        }
+    }
+}
---
+++ Scripts/VivoxManager_Client.cs
@@ -0,0 +1,93 @@
+#if !UNITY_SERVER
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Core;
+using Unity.Services.Vivox;
+
+namespace Insthync.UnityVivoxIntegration
+{
+    public partial class VivoxManager : IVivoxTokenProvider
+    {
+        private async void InitializeForClient()
+        {
+            var options = new InitializationOptions();
+            if (CheckManualCredentials())
+            {
+                options.SetVivoxCredentials(_server, _domain, _issuer, _key);
+            }
+            VivoxService.Instance.SetTokenProvider(this);
+            await UnityServices.InitializeAsync(options);
+            await VivoxService.Instance.InitializeAsync();
+        }
+
+        private bool CheckManualCredentials()
+        {
+            return !(string.IsNullOrWhiteSpace(_issuer) && string.IsNullOrWhiteSpace(_domain) && string.IsNullOrWhiteSpace(_server));
+        }
+
+        public Task<string> GetTokenAsync(string issuer = null, TimeSpan? expiration = null, string targetUserUri = null, string action = null, string channelUri = null, string fromUserUri = null, string realm = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToggleMicrophone()
+        {
+            if (VivoxService.Instance.IsInputDeviceMuted)
+                VivoxService.Instance.UnmuteInputDevice();
+            else
+                VivoxService.Instance.MuteInputDevice();
+        }
+
+        public void ToggleSpeaker()
+        {
+            if (VivoxService.Instance.IsOutputDeviceMuted)
+                VivoxService.Instance.UnmuteOutputDevice();
+            else
+                VivoxService.Instance.MuteOutputDevice();
+        }
+
+        public void MuteMicrophone()
+        {
+            VivoxService.Instance.MuteInputDevice();
+        }
+
+        public void UnmuteMicrophone()
+        {
+            VivoxService.Instance.UnmuteInputDevice();
+        }
+
+        public void MuteSpeaker()
+        {
+            VivoxService.Instance.MuteOutputDevice();
+        }
+
+        public void UnmuteSpeaker()
+        {
+            VivoxService.Instance.UnmuteOutputDevice();
+        }
+
+        /// <summary>
+        /// Sets the input device volume for the local user. This applies to all active audio sessions. Volume value is clamped between -50 and 50 with a default of 0.
+        /// </summary>
+        /// <param name="volume"></param>
+        public void SetMicrophoneVolume(int volume = 0)
+        {
+            VivoxService.Instance.SetInputDeviceVolume(volume);
+        }
+
+        /// <summary>
+        /// Sets the output device volume for the local user. This applies to all active audio sessions. Volume value is clamped between -50 and 50 with a default of 0.
+        /// </summary>
+        /// <param name="volume"></param>
+        public void SetSpeakerVolume(int volume = 0)
+        {
+            VivoxService.Instance.SetOutputDeviceVolume(volume);
+        }
+
+        public bool IsMicrophoneMuted => VivoxService.Instance.IsInputDeviceMuted;
+        public bool IsSpeakerMuted => VivoxService.Instance.IsOutputDeviceMuted;
+        public int MicrophoneVolume => VivoxService.Instance.InputDeviceVolume;
+        public int SpeakerVolume => VivoxService.Instance.OutputDeviceVolume;
+    }
+}
+#endif
