d2ee90b302789bcdf24f67c8140253d3003b00d9
insthync <ittipon.bay@gmail.com>
2024-09-28 02:04:29 +0700

    feat: add function to get channel type from channel uri

--- Scripts/VivoxExtensions.cs
+++ Scripts/VivoxExtensions.cs
@@ -6,14 +6,21 @@ namespace Insthync.UnityVivoxIntegration
         {
             switch (channelType)
             {
-                case VivoxChannelType.NonPositional:
-                    return "confctl-g-";
                 case VivoxChannelType.Positional:
                     return "confctl-d-";
                 case VivoxChannelType.Echo:
                     return "confctl-e-";
             }
             return "confctl-g-";
+        }
+
+        public static VivoxChannelType GetChannelType(this string channelUri)
+        {
+            if (channelUri.StartsWith("sip:confctl-d-") || channelUri.StartsWith("confctl-d-"))
+                return VivoxChannelType.Positional;
+            else if (channelUri.StartsWith("sip:confctl-e-") || channelUri.StartsWith("confctl-e-"))
+                return VivoxChannelType.Echo;
+            return VivoxChannelType.NonPositional;
         }
     }
 }
